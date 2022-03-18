1c5a3bdcf8ba188d8af04430240fd5f437c5a80a
soohoonlee <jjinimania@gmail.com>
2022-03-18 16:29:02 +0900

    Ticket Reservation System

---
+++ chapter02/src/main/java/movie/Customer.java
@@ -0,0 +1,11 @@
+package movie;
+
+public class Customer {
+	private final String name;
+	private final String id;
+
+	public Customer(String name, String id) {
+		this.name = name;
+		this.id = id;
+	}
+}
---
+++ chapter02/src/test/java/Test.java
@@ -0,0 +1,32 @@
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalTime;
+
+import money.Money;
+import movie.Movie;
+import movie.pricing.AmountDiscountPolicy;
+import movie.pricing.PercentDiscountPolicy;
+import movie.pricing.PeriodCondition;
+import movie.pricing.SequenceCondition;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Movie avatar = new Movie("아바타",
+				Duration.ofMinutes(120),
+				Money.wons(10000),
+				new AmountDiscountPolicy(Money.wons(800),
+						new SequenceCondition(1),
+						new SequenceCondition(10),
+						new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(11, 59)),
+						new PeriodCondition(DayOfWeek.THURSDAY, LocalTime.of(10, 0), LocalTime.of(20, 59))));
+
+		Movie titanic = new Movie("타이타닉",
+				Duration.ofMinutes(180),
+				Money.wons(11000),
+				new PercentDiscountPolicy(0.1,
+						new PeriodCondition(DayOfWeek.TUESDAY, LocalTime.of(14, 0), LocalTime.of(16, 59)),
+						new SequenceCondition(2),
+						new PeriodCondition(DayOfWeek.THURSDAY, LocalTime.of(10, 0), LocalTime.of(13, 59))));
+	}
+}
