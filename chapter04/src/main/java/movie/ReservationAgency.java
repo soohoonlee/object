003dea43a2e79c0fa3418e640fe15d220cbafff8
soohoonlee <jjinimania@gmail.com>
2022-03-24 17:27:58 +0900

    Enhanced Data Driven Movie Reservation System

--- chapter04/src/main/java/movie/ReservationAgency.java
+++ chapter04/src/main/java/movie/ReservationAgency.java
@@ -5,35 +5,7 @@ import money.Money;
 public class ReservationAgency {
 
 	public Reservation reserve(Screening screening, Customer customer, int audienceCount) {
-		Movie movie = screening.getMovie();
-
-		boolean discountable = false;
-		for (DiscountCondition condition : movie.getDiscountConditions()) {
-			if (condition.getType() == DiscountConditionType.PERIOD) {
-				discountable = condition.getDayOfWeek().equals(screening.getWhenScreened().getDayOfWeek()) &&
-						condition.getStartTime().compareTo(screening.getWhenScreened().toLocalTime()) <= 0 &&
-						condition.getEndTime().compareTo(screening.getWhenScreened().toLocalTime()) >= 0;
-			}
-			else {
-				discountable = condition.getSequence() == screening.getSequence();
-			}
-
-			if (discountable) {
-				break;
-			}
-		}
-
-		Money fee;
-		if (discountable) {
-			Money discountAmount = switch (movie.getMovieType()) {
-				case AMOUNT_DISCOUNT -> movie.getDiscountAmount();
-				case PERCENT_DISCOUNT -> movie.getFee().times(movie.getDiscountPercent());
-				case NONE_DISCOUNT -> Money.ZERO;
-			};
-			fee = movie.getFee().minus(discountAmount).times(audienceCount);
-		} else {
-			fee = movie.getFee();
-		}
+		Money fee = screening.calculateFee(audienceCount);
 		return new Reservation(customer, screening, fee, audienceCount);
 	}
 }
--- chapter04/src/main/java/movie/Screening.java
+++ chapter04/src/main/java/movie/Screening.java
@@ -2,6 +2,8 @@ package movie;
 
 import java.time.LocalDateTime;
 
+import money.Money;
+
 public class Screening {
 
 	private Movie movie;
@@ -30,5 +32,23 @@ public class Screening {
 
 	public void setWhenScreened(LocalDateTime whenScreened) {
 		this.whenScreened = whenScreened;
+	}
+
+	public Money calculateFee(int audienceCount) {
+		switch (movie.getMovieType()) {
+			case AMOUNT_DISCOUNT:
+				if (movie.isDiscountable(whenScreened, sequence)) {
+					return movie.calculateAmountDiscountedFee().times(audienceCount);
+				}
+				break;
+			case PERCENT_DISCOUNT:
+				if (movie.isDiscountable(whenScreened, sequence)) {
+					return movie.calculatePercentDiscountedFee().times(audienceCount);
+				}
+				break;
+			case NONE_DISCOUNT:
+				return movie.calculateNoneDiscountedFee().times(audienceCount);
+		}
+		return movie.calculateNoneDiscountedFee().times(audienceCount);
 	}
 }
