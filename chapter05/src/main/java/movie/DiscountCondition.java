5db647c82373c80aa4e7fe27130f0b9acb8e0ed0
soohoonlee <jjinimania@gmail.com>
2022-03-29 00:21:29 +0900

    Type separation

--- chapter05/src/main/java/movie/DiscountCondition.java
+++ chapter05/src/main/java/movie/DiscountCondition.java
@@ -1,26 +1,15 @@
 package movie;
 
-import java.time.DayOfWeek;
-import java.time.LocalTime;
-
 public class DiscountCondition {
+	private final PeriodCondition periodCondition = new PeriodCondition(this);
 	private DiscountConditionType type;
 	private int sequence;
-	private DayOfWeek dayOfWeek;
-	private LocalTime startTime;
-	private LocalTime endTime;
 
 	public boolean isSatisfiedBy(Screening screening) {
 		if (type == DiscountConditionType.PERIOD) {
-			return isSatisfiedByPeriod(screening);
+			return periodCondition.isSatisfiedBy(screening);
 		}
 		return isSatisfiedBySequence(screening);
-	}
-
-	private boolean isSatisfiedByPeriod(Screening screening) {
-		return screening.getWhenScreened().getDayOfWeek().equals(dayOfWeek) &&
-				screening.getWhenScreened().toLocalTime().compareTo(startTime) >= 0 &&
-				screening.getWhenScreened().toLocalTime().compareTo(endTime) <= 0;
 	}
 
 	private boolean isSatisfiedBySequence(Screening screening) {
--- chapter05/src/main/java/movie/Movie.java
+++ chapter05/src/main/java/movie/Movie.java
@@ -7,13 +7,22 @@ import money.Money;
 
 public class Movie {
 	private String title;
-	private Duration duration;
+	private Duration runningTime;
 	private Money fee;
-	private List<DiscountCondition> discountConditions;
+	private List<PeriodCondition> periodConditions;
+	private List<SequenceCondition> sequenceConditions;
 
 	private MovieType movieType;
 	private Money discountAmount;
 	private double discountPercent;
+
+	public Movie(String title, Duration runningTime, Money fee, List<PeriodCondition> periodConditions, List<SequenceCondition> sequenceConditions) {
+		this.title = title;
+		this.runningTime = runningTime;
+		this.fee = fee;
+		this.periodConditions = periodConditions;
+		this.sequenceConditions = sequenceConditions;
+	}
 
 	public Money calculateMovieFee(Screening screening) {
 		if (isDiscountable(screening)) {
@@ -23,7 +32,16 @@ public class Movie {
 	}
 
 	private boolean isDiscountable(Screening screening) {
-		return discountConditions.stream()
+		return checkPeriodConditions(screening) || checkSequenceConditions(screening);
+	}
+
+	private boolean checkPeriodConditions(Screening screening) {
+		return periodConditions.stream()
+				.anyMatch(condition -> condition.isSatisfiedBy(screening));
+	}
+
+	private boolean checkSequenceConditions(Screening screening) {
+		return sequenceConditions.stream()
 				.anyMatch(condition -> condition.isSatisfiedBy(screening));
 	}
 
