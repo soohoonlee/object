9ab26325b98fca66f93307a5a3966e46a43f5864
soohoonlee <jjinimania@gmail.com>
2022-04-04 14:43:32 +0900

    Initial Event

---
+++ chapter06/src/main/java/event/Event.java
@@ -0,0 +1,16 @@
+package event;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class Event {
+	private String subject;
+	private LocalDateTime from;
+	private Duration duration;
+
+	public Event(String subject, LocalDateTime from, Duration duration) {
+		this.subject = subject;
+		this.from = from;
+		this.duration = duration;
+	}
+}
