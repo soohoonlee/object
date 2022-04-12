abd91751a3b1117d308dfa391dc5eb6ee2c2b5c6
soohoonlee <jjinimania@gmail.com>
2022-04-13 08:08:16 +0900

    add taxRate

--- chapter10/src/main/java/billing/Phone.java
+++ chapter10/src/main/java/billing/Phone.java
@@ -6,14 +6,19 @@ import java.util.List;
 import money.Money;
 
 public abstract class Phone {
+	private double taxRate;
 	private List<Call> calls = new ArrayList<>();
+
+	protected Phone(double taxRate) {
+		this.taxRate = taxRate;
+	}
 
 	public Money calculateFee() {
 		Money result = Money.ZERO;
 		for (Call call : calls) {
 			result = result.plus(calculateCallFee(call));
 		}
-		return result;
+		return result.plus(result.times(taxRate));
 	}
 
 	protected abstract Money calculateCallFee(Call call);
