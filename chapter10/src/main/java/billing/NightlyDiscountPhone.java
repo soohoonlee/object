bfdc1975f944d619ca60e1237a27bcb270c9231b
soohoonlee <jjinimania@gmail.com>
2022-04-12 17:07:19 +0900

    change NightlyDiscountPhone to subClass

---
+++ chapter10/src/main/java/billing/NightlyDiscountPhone.java
@@ -0,0 +1,28 @@
+package billing;
+
+import java.time.Duration;
+
+import money.Money;
+
+public class NightlyDiscountPhone extends Phone {
+	private static final int LATE_NIGHT_HOUR = 22;
+	private Money nightlyAmount;
+
+	public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds) {
+		super(regularAmount, seconds);
+		this.nightlyAmount = nightlyAmount;
+	}
+
+	@Override
+	public Money calculateFee() {
+		// 부모 클래스의 calculateFee 호출
+		Money result = super.calculateFee();
+		Money nightlyFee = Money.ZERO;
+		for (Call call : getCalls()) {
+			if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
+				nightlyFee = result.plus(getAmount().minus(nightlyAmount).times((double) call.getDuration().getSeconds() / getSeconds().getSeconds()));
+			}
+		}
+		return result.minus(nightlyFee);
+	}
+}
--- chapter10/src/main/java/billing/Phone.java
+++ chapter10/src/main/java/billing/Phone.java
@@ -7,32 +7,12 @@ import java.util.List;
 import money.Money;
 
 public class Phone {
-	private static final int LATE_NIGHT_HOUR = 22;
-
-	enum PhoneType {
-		REGULAR,
-		NIGHTLY
-	}
-	private PhoneType type;
 	private Money amount;
-	private Money nightlyAmount;
-	private Money regularAmount;
 	private Duration seconds;
 	private List<Call> calls = new ArrayList<>();
 
 	public Phone(Money amount, Duration seconds) {
-		this(PhoneType.REGULAR, amount, Money.ZERO, Money.ZERO, seconds);
-	}
-
-	public Phone(Money nightlyAmount, Money regularAmount, Duration seconds) {
-		this(PhoneType.NIGHTLY, Money.ZERO, nightlyAmount, regularAmount, seconds);
-	}
-
-	public Phone(PhoneType type, Money amount, Money nightlyAmount, Money regularAmount, Duration seconds) {
-		this.type = type;
 		this.amount = amount;
-		this.nightlyAmount = nightlyAmount;
-		this.regularAmount = regularAmount;
 		this.seconds = seconds;
 	}
 
@@ -55,17 +35,7 @@ public class Phone {
 	public Money calculateFee() {
 		Money result = Money.ZERO;
 		for (Call call : calls) {
-			if (type == PhoneType.REGULAR) {
-				result = result.plus(amount.times((double) call.getDuration().getSeconds() / seconds.getSeconds()));
-			}
-			else {
-				if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
-					result = result.plus(nightlyAmount.times((double) call.getDuration().getSeconds() / seconds.getSeconds()));
-				}
-				else {
-					result = result.plus(regularAmount.times((double) call.getDuration().getSeconds() / seconds.getSeconds()));
-				}
-			}
+			result = result.plus(amount.times((double) call.getDuration().getSeconds() / seconds.getSeconds()));
 		}
 		return result;
 	}
