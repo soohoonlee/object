5c4255300b47ec7b6aaacdc97c155e131e821bd5
soohoonlee <jjinimania@gmail.com>
2022-04-12 15:54:09 +0900

    Initial PhoneBilling

---
+++ chapter10/src/main/java/money/Money.java
@@ -0,0 +1,52 @@
+package money;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Money {
+	public static final Money ZERO = Money.wons(0);
+
+	private final BigDecimal amount;
+
+	private static Money wons(long amount) {
+		return new Money(BigDecimal.valueOf(amount));
+	}
+
+	private static Money wons(double amount) {
+		return new Money(BigDecimal.valueOf(amount));
+	}
+
+	public Money(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+	public Money plus(Money amount) {
+		return new Money(this.amount.add(amount.amount));
+	}
+
+	public Money minus(Money amount) {
+		return new Money(this.amount.subtract(amount.amount));
+	}
+
+	public Money times(double percent) {
+		return new Money(this.amount.multiply(BigDecimal.valueOf(percent)));
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Money money = (Money) o;
+		return Objects.equals(amount.doubleValue(), money.amount.doubleValue());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(amount);
+	}
+
+	@Override
+	public String toString() {
+		return amount.toString() + "원";
+	}
+}
