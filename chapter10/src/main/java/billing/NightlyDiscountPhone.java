93815004ecba2338862bac9028ccf59b0339ad68
soohoonlee <jjinimania@gmail.com>
2022-04-12 23:59:09 +0900

    abstract class

--- chapter10/src/main/java/billing/NightlyDiscountPhone.java
+++ chapter10/src/main/java/billing/NightlyDiscountPhone.java
@@ -6,12 +6,11 @@ import java.util.List;
 
 import money.Money;
 
-public class NightlyDiscountPhone {
+public class NightlyDiscountPhone extends AbstractPhone {
 	private static final int LATE_NIGHT_HOUR = 22;
 	private Money nightlyAmount;
 	private Money regularAmount;
 	private Duration seconds;
-	private List<Call> calls = new ArrayList<>();
 
 	public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds) {
 		this.nightlyAmount = nightlyAmount;
@@ -19,15 +18,8 @@ public class NightlyDiscountPhone {
 		this.seconds = seconds;
 	}
 
-	public Money calculateFee() {
-		Money result = Money.ZERO;
-		for (Call call : calls) {
-			result = result.plus(calculateCallFee(call));
-		}
-		return result;
-	}
-
-	private Money calculateCallFee(Call call) {
+	@Override
+	protected Money calculateCallFee(Call call) {
 		if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
 			return nightlyAmount.times((double) call.getDuration().getSeconds() / seconds.getSeconds());
 		}
--- chapter10/src/main/java/billing/Phone.java
+++ chapter10/src/main/java/billing/Phone.java
@@ -6,25 +6,17 @@ import java.util.List;
 
 import money.Money;
 
-public class Phone {
+public class Phone extends AbstractPhone {
 	private Money amount;
 	private Duration seconds;
-	private List<Call> calls = new ArrayList<>();
 
 	public Phone(Money amount, Duration seconds) {
 		this.amount = amount;
 		this.seconds = seconds;
 	}
 
-	public Money calculateFee() {
-		Money result = Money.ZERO;
-		for (Call call : calls) {
-			result = result.plus(calculateCallFee(call));
-		}
-		return result;
-	}
-
-	private Money calculateCallFee(Call call) {
+	@Override
+	protected Money calculateCallFee(Call call) {
 		return amount.times((double) call.getDuration().getSeconds() / seconds.getSeconds());
 	}
 }
