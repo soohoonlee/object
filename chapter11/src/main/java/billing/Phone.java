6f83a0ddadbfb86aee0102cd2f7b4b30e0279947
soohoonlee <jjinimania@gmail.com>
2022-04-13 18:31:56 +0900

    Initial PhoneBilling

---
+++ chapter11/src/main/java/billing/Phone.java
@@ -0,0 +1,21 @@
+package billing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import money.Money;
+
+public abstract class Phone {
+	private List<Call> calls = new ArrayList<>();
+
+	public Money calculateFee() {
+		Money result = Money.ZERO;
+
+		for (Call call : calls) {
+			result = result.plus(calculateCallFee(call));
+		}
+		return result;
+	}
+
+	protected abstract Money calculateCallFee(Call call);
+}
