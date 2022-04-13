f2ced397a8b13434c654b70fb0788c23cdc1797b
soohoonlee <jjinimania@gmail.com>
2022-04-13 23:18:29 +0900

    add RateDiscountable

---
+++ chapter11/src/main/java/billing/RateDiscountableNightlyDiscountPhone.java
@@ -0,0 +1,19 @@
+package billing;
+
+import java.time.Duration;
+
+import money.Money;
+
+public class RateDiscountableNightlyDiscountPhone extends NightlyDiscountPhone {
+	private Money discountAmount;
+
+	public RateDiscountableNightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds, Money discountAmount) {
+		super(nightlyAmount, regularAmount, seconds);
+		this.discountAmount = discountAmount;
+	}
+
+	@Override
+	protected Money afterCalculated(Money fee) {
+		return fee.minus(discountAmount);
+	}
+}
