7a37812f824a7e382f1700f3ba523ed655921b12
soohoonlee <jjinimania@gmail.com>
2022-04-14 16:01:36 +0900

    add Professor(upcasting, dynamic binding)

--- chapter12/src/main/java/lecture/Test.java
+++ chapter12/src/main/java/lecture/Test.java
@@ -14,5 +14,19 @@ public class Test {
 				Arrays.asList(81, 95, 75, 50, 45));
 
 		System.out.println("lecture = " + lecture.evaluate());
+
+		Professor professor = new Professor("다익스트라", new Lecture("자료구조", 70, Arrays.asList(81, 95, 75, 50, 45)));
+		System.out.println("professor = " + professor.compileStatistics());
+
+		Professor professor1 = new Professor("다익스트라",
+				new GradeLecture("자료구조",
+						70,
+						Arrays.asList(new Grade("A", 100, 95),
+								new Grade("B", 94, 80),
+								new Grade("C", 79, 70),
+								new Grade("D", 69, 50),
+								new Grade("F", 49, 0)),
+						Arrays.asList(81, 95, 75, 50, 45)));
+		System.out.println("professor1 = " + professor1.compileStatistics());
 	}
 }
